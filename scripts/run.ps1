4532cf6fb63a06302ae53372acd5be723c6df6d5
filippetran <filippetran@gmail.com>
2024-03-18 12:25:30 +0100

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -34,6 +34,10 @@ switch ($command) {
     "mongo" {
       mongo up
     }
+
+    "docker" {
+      docker build -t filippetran/ambulance-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+   }
     
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
